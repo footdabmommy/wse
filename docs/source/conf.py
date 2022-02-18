658a5eea977e302f621a8fe2a4753c658431aba0
insert username here <montyd18@gmail.com>
2022-02-17 21:21:59 -0500

    fix name here too

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Worst Server Ever Docs'
+copyright = '2022, Worst Server Ever'
+author = 'Jarool'
 
 release = '0.1'
 version = '0.1.0'
