8ec921da618ee0e3cc654089fa3571c16768ea3f
Mdog504 <60322646+Mdog504@users.noreply.github.com>
2022-04-22 23:01:29 +0100

    Smol

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,7 +34,7 @@ html_title = 'Worst Server Ever Documentation'
 html_short_title = 'WSE Docs'
 html_favicon = 'WSE-icon.ico'
 html_theme = 'sphinx_rtd_theme'
-html_logo = 'https://media.discordapp.net/attachments/877207380625096734/966768887603548180/WSE_3.png'
+html_logo = 'https://media.discordapp.net/attachments/801499825275011093/967183074028814366/WSE_smol.png'
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
